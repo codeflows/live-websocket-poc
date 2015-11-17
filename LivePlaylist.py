9249bd83ba2e83723f6ed2a219743ceaa3c66837
Jari Aarniala <jari.aarniala@gmail.com>
2015-11-17 21:52:32 +0200

    Play handler

--- LivePlaylist.py
+++ LivePlaylist.py
@@ -7,4 +7,8 @@ class LivePlaylist(ControlSurface):
         ControlSurface.__init__(self, c_instance)
         with self.component_guard():
             self.__c_instance = c_instance
-            self.show_message('test')
+            self.log_message('LivePlaylist starting up')
+            self.song().add_is_playing_listener(self.__playing_status_changed)
+
+    def __playing_status_changed(self):
+        self.log_message('Playing? %s' % self.song().is_playing)
