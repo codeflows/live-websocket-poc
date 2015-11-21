d4f34bd4bb4cec6aa6d620bd32e64f3dc25374d4
Jari Aarniala <jari.aarniala@gmail.com>
2015-11-21 14:42:26 +0200

    Move source

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 echo "Syncing LivePlaylist to Ableton remote scripts folder..."
-rsync -rt . /Applications/Ableton\ Live\ 9.5\ Beta.app/Contents/App-Resources/MIDI\ Remote\ Scripts/LivePlaylist/
+rsync -rt src /Applications/Ableton\ Live\ 9.5\ Beta.app/Contents/App-Resources/MIDI\ Remote\ Scripts/LivePlaylist/
