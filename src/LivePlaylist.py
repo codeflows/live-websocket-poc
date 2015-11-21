db99b33cdca022f7f433b271e2996aebba8a34ff
Jari Aarniala <jari.aarniala@gmail.com>
2015-11-21 15:50:04 +0200

    Revert "Thread experiment"
    This reverts commit dc795f3e9564e6b558b221fa9538085703919d42.

--- src/LivePlaylist.py
+++ src/LivePlaylist.py
@@ -2,7 +2,6 @@ from __future__ import with_statement
 import Live
 from _Framework.ControlSurface import ControlSurface
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
-import threading
 
 class SimpleEcho(WebSocket):
    def handleMessage(self):
@@ -28,25 +27,6 @@ class LivePlaylist(ControlSurface):
             self.log_message(platform.python_version())
 
             self.server = SimpleWebSocketServer("", 55455, SimpleEcho)
-            self.log_message('Started WS server')
-
-            thread = threading.Thread(target=self.thread_run, args=())
-            thread.daemon = True
-            thread.start()
-            self.log_message('Started background thread')
-
-    def thread_run(self):
-        self.log_message('THRIID!')
-
-        """ Method that runs forever """
-        while True:
-            myFile = open('/Users/jaarnial/Projects/LivePlaylist/LivePlaylist/DEADBEEF.txt', 'w')
-            myFile.write('remix.net test 1 2 3\n')
-            myFile.close
-
-            self.log_message('Doing something imporant in the background')
-
-            time.sleep(2)
 
     def __cue_points_changed(self):
         for cp in self.song().cue_points:
